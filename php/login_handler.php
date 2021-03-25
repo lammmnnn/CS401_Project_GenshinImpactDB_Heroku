3b7c3cc3ec8a8f1a6168723733c131cf3d8433d2
lammmnnn <41709598+lammmnnn@users.noreply.github.com>
2021-03-24 19:35:45 -0600

    Update login_handler.php

--- php/login_handler.php
+++ php/login_handler.php
@@ -6,7 +6,17 @@
 
   // check the email and password
   require_once 'Dao.php';
-  
-  header('Location: /php/Dao.php');
+  $dao = new Dao();
+
+  $_SESSION['form_data'] = $_POST;
+  $_SESSION['authenticated'] = $dao->loginMatch($username, $password);
+
+  if ($_SESSION['authenticated']) {
+    header('Location: /index.php');
     exit;
+  } else {
+    $_SESSION['loginError'] = true;
+    header('Location: /php/login.php');
+    exit;
+  }
  
