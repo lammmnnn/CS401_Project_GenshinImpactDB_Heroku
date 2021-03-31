6f4b07bc590e3b5cc757134bc355938e4800b4c6
lammmnnn <41709598+lammmnnn@users.noreply.github.com>
2021-03-30 22:52:09 -0600

    Add files via upload

--- php/user_setting.php
+++ php/user_setting.php
@@ -1,80 +1,68 @@
-<!DOCTYPE php>
 <?php
   session_start();
 ?>
-
+<!DOCTYPE php>
 <html>
 <head>
     <title>Genshin Impact Database | XLK</title>
-    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="shortcut icon" type="image/jpg" href="../img/favicon.ico">
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="/css/login.css">
 </head>
 <body class="userauthen">
   <div class="topnav">
     <div class="logo">
-      <a href="/index.php"><img src="/img/mainlogo2.png" height="64" alt=''></a>
+      <a href="/php/index.php"><img src="/img/mainlogo2.png" height="50" alt=''></a>
     </div>
     <h2>Database by <span style="color: #007BEF;">XLK</span></h2>
-    <a class="loginbutton" <?php echo isset($_SESSION['form_data'])
-     ? '' : 'href="/php/login.php"' ; ?>><?php echo isset($_SESSION['form_data'])
-     ? $_SESSION['form_data']['username'] : 'Sign in' ; ?></a>
+    <div class="login-div <?php echo isset($_SESSION['username'])
+     ? ' dropdown'  : '' ; ?>">
+      <a class="login-button" <?php echo isset($_SESSION['username'])
+       ? 'style="text-decoration: underline;">' . $_SESSION['username']
+        : 'href="/php/login.php">Sign in' ; ?></a>
+      <div class="dropdown-content">
+        <a href="/php/user_setting.php">Profile Setting</a>
+        <a href="/handler/logout_handler.php">Log out</a>
+      </div>
+    </div>
   </div>
-  <div id="navbar">
-    <div class="items">
-      <a href="/index.php" class="button first">HOME</a>
-      <div class="dropdown">
-        <a class="button">CHARACTERS</a>
-        <div class="dropdown-content">
-          <a href="/php/pcharacter.php">Playable Characters</a>
-          <a href="/php/ucharacter.php">Unreleased Characters</a>
-        </div>
-      </div>
-      <div class="dropdown">
-        <a class="button">ITEMS</a>
-        <div class="dropdown-content">
-          <a href="#">Link 1</a>
-        </div>
-      </div>
-      <a href="" class="button second">GUIDES</a>
-      <a href="" class="button">DPS CALCULATOR</a>
-      <a href="" class="button">WORLD MAP</a>
-    </div>
 
-    <div class="search">
-      <input type="search" placeholder="Search">
-      <label class="icon">
-        <span class="fas fa-search"></span>
-      </label>
-    </div>
 
   </div>
   <main>
-  <div>
+  <div class="us-panel">
+    <div class="content-header">
+      <h6>Account Information</h6>
+    </div>
+    <div class="content-body">
+      <p class="account-info-item">
+        <span class="account-info-label">Username:</span>
+        <span><?php echo $_SESSION['username']; ?></span>
+        </p>
+        <p class="account-info-item">
+          <span class="account-info-label">Password:</span>
 
-
+        </p>
+        <p class="account-info-item">
+          <span class="account-info-label">Email:</span>
+          <span><?php echo $_SESSION['email']; ?></span>
+        </p>
+      </div>
+    </div>
   </div>
   </main>
 
-  <div class="footer">
-    <p>Footer</p>
-  </div>
+  <footer class="footer">
+    <hr>
+    <ul class="footer__nav black">
+      <a href="/about"> About </a>
+      <a href="/contact"> Contact </a>
+      <a href="/disclaimer"> Disclaimer </a>
+    </ul>
+    <p><span>©&nbsp;2021&nbsp; Genshin Impact Database</span></p>
+  </footer>
 
 <!--  <script>resetCookies();</script>  -->
-
-<!-- MAKE NAVBAR STICKY -->
-    <script>
-    window.onscroll = function() {myFunction()};
-
-    var navbar = document.getElementById("navbar");
-    var sticky = navbar.offsetTop;
-
-    function myFunction() {
-      if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-      } else {
-        navbar.classList.remove("sticky");
-      }
-    }
-    </script>
 
 </body>
 </html>
